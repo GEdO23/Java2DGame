1e1e4e193273be1a1f33453cda32292a891bc868
GEdO23 <rm99632@fiap.com.br>
2024-02-19 18:52:21 -0300

    Starting gameThread

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -4,7 +4,7 @@ import javax.swing.JPanel;
 import java.awt.Dimension;
 import java.awt.Color;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 
     // SCREEN SETTINGS
     final int originalTileSize = 16; // 16x16 tile
@@ -16,9 +16,21 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels
 
+    Thread gameThread;
+
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true); // Double buffering to improve rendering performance
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
     }
 }
