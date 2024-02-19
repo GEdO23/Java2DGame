d0cb4f2e41ee32f3884dbe3590008d0f72f79d78
GEdO23 <rm99632@fiap.com.br>
2024-02-19 18:31:18 -0300

    Creating a window

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,19 @@
 package main;
 
+import javax.swing.JFrame;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        JFrame window = new JFrame();
+        // Will let the window properly close with the close button
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("2D Adventure");
+        // When window location is not specified, display's at center of the screen
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
 
     }
 
