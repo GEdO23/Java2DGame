318228a9afb330c13fd50c9f00dc2a001d3b1cc4
GEdO23 <rm99632@fiap.com.br>
2024-02-19 18:41:56 -0300

    Creating the GamePanel

---
+++ src/main/GamePanel.java
@@ -0,0 +1,24 @@
+package main;
+
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.Color;
+
+public class GamePanel extends JPanel {
+
+    // SCREEN SETTINGS
+    final int originalTileSize = 16; // 16x16 tile
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale; // 48x48 tile
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
+    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true); // Double buffering to improve rendering performance
+    }
+}
