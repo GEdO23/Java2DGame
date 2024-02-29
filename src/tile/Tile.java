dc761e3d48ddee418ff604c7362e08e9acbb257f
GEdO23 <rm99632@fiap.com.br>
2024-02-29 19:38:01 -0300

    Removendo GrassTile.java e colocando o método getTiles diretamente no Tile.java

--- src/tile/Tile.java
+++ src/tile/Tile.java
@@ -1,10 +1,53 @@
 package tile;
 
+import main.ImageReader;
+
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Tile {
 
     public BufferedImage image;
     public boolean collision = false;
 
+    public Tile() {
+    }
+
+    public Tile(BufferedImage image) {
+        this.image = image;
+    }
+
+    public Tile(BufferedImage image, boolean collision) {
+        this.image = image;
+        this.collision = collision;
+    }
+
+    public static ArrayList<Tile> getTiles(String folderPath) {
+
+        File folder = new File("res/" + folderPath);
+        File[] files = folder.listFiles();
+
+        Tile[] tile = new Tile[files.length];
+
+        ArrayList<Tile> tileSet = new ArrayList<>();
+
+        try {
+            String filePath;
+
+            for (int i = 0; i < files.length; i++) {
+                tile[i] = new Tile();
+                filePath = folderPath + "/tile" + (i+1) + ".png";
+                System.out.println(filePath);
+                tile[i].image = ImageReader.readImage(filePath);
+                tileSet.add( tile[i] );
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return tileSet;
+    }
 }
--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -1,31 +1,23 @@
 package tile;
 
 import main.GamePanel;
-import main.ImageReader;
-
-import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
 
 public class TileManager {
 
     GamePanel gp;
     ArrayList< ArrayList<Tile> > tiles = new ArrayList<>();
-    String[][] mapTileNum;
-    int tileType;
-    int tileIndex = 0;
+    int[][] mapTileNum;
+
 
     public TileManager(GamePanel gp) {
         this.gp = gp;
 
-        mapTileNum = new String[gp.maxScreenCol][gp.maxScreenRow];
+        mapTileNum = new int[gp.maxScreenCol][gp.maxScreenRow];
 
         getTileImage();
         loadMap("/maps/map03.txt");
@@ -33,9 +25,8 @@ public class TileManager {
 
     public void getTileImage() {
 
-        tiles.add(GrassTile.getGrassTiles());
-
-
+        tiles.add(Tile.getTiles("/tiles/grass_dirt"));
+//        tiles.add(Tile.getTiles("/tiles/concrete_path"));
     }
 
 
@@ -43,88 +34,30 @@ public class TileManager {
 
         try {
 
-
             // Reading the map file
             InputStream inputStream = getClass().getResourceAsStream(filePath);
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 
-            /*
-            System.out.println("Class: " + getClass());
-            System.out.println("FilePath: " + filePath);
-            System.out.println("InputStream: " + inputStream);
-            System.out.println("BufferedReader: " + bufferedReader); // String
+            int row = 0;
+            String line;
+            while (( line = bufferedReader.readLine() )!= null) {
 
-            String line = bufferedReader.readLine();
-            System.out.println("Linha 1: " + line);
-            String[] column = line.split(" ");
-            System.out.println("Coluna 1: " + column[0]);
-            String[] code = column[0].split("-");
-            int tileType = Integer.parseInt(code[0]);
-            int tileIndex = Integer.parseInt(code[1]);
-            System.out.println("TileType: " + tileType);
-            System.out.println("TileIndex: " + tileIndex);
-            */
+                String[] column = line.split(" ");
 
+                for (int col = 0; col < gp.maxScreenCol; col++) {
 
+                    String[] code = column[col].split("-");
+                    int tileTypeId = Integer.parseInt(code[0]);
+                    int tileIndex = Integer.parseInt(code[1]);
 
+                    mapTileNum[col][row] = tileTypeId * 10 + tileIndex;
 
-            /* Leia o arquivo de texto
+                }
 
-            Cada linha do arquivo será adcionada a uma lista
-            [
-                linha1,
-                linha2,
-                linha3
-            ]
+                row++;
 
-            Cada linhas possui colunas, iremos identificar essas colunas e colocar na lista linha
-            [
-                [ coluna1, coluna2, coluna3 ],
-                [ coluna1, coluna2, coluna3 ],
-                [ coluna1, coluna2, coluna3 ]
-            ]
+            }
 
-            Cada coluna possui um código com o seguinte formato x-y
-            O x é o tipo de tile (grama, agua, pedra)
-            O y é o index do tile (posição ou formato do tile)
-
-            Devo dividir cada coluna em uma lista com o x e o y
-            [
-                [ [ x, y ], [ x, y ], [ x, y ] ],
-                [ [ x, y ], [ x, y ], [ x, y ] ],
-                [ [ x, y ], [ x, y ], [ x, y ] ]
-            ]
-
-            arquivo;
-            linha1 = arquivo[0];
-            coluna1 = linha1[0];
-            x1 = coluna1[0]; // tileType
-            y1 = coluna1[1]; // tileIndex
-
-            // [linha][coluna][opção]
-            x1 = arquivo[0][0][0];
-            y1 = arquivo[0][0][1];
-
-            tiles.get( arquivo[0][0][0] ).get( arquivo[0][0][1] ).image
-            tiles.get(tileType).get(tileIndex).image
-             */
-
-//            int col = 0;
-//            int row = 0;
-//            while (row < gp.maxScreenRow) {
-//
-//                String line = bufferedReader.readLine();
-//
-//                while (col < gp.maxScreenCol) {
-//                    String[] numbers = line.split(" ");
-//                    int num = Integer.parseInt(numbers[col]);
-//
-//                    mapTileNum[col][row] = num;
-//                    col++;
-//                }
-//                col = 0;
-//                row++;
-//            }
             bufferedReader.close();
 
         } catch (Exception e) {
@@ -135,23 +68,27 @@ public class TileManager {
 
     public void draw(Graphics2D g2) {
 
-        int col = 0;
-        int row = 0;
-        int x = 0;
-        int y = 0;
 
-        while (col < gp.maxScreenCol && row < gp.maxScreenRow) {
+        for (int row = 0; row < gp.maxScreenRow; row++) {
+            for (int col = 0; col < gp.maxScreenCol; col++) {
 
-            g2.drawImage(
-                    tiles.get(tileType).get(tileIndex).image, x, y, gp.tileSize, gp.tileSize, null);
-            col++;
-            x += gp.tileSize;
+                int tileCode = mapTileNum[col][row];
 
-            if(col == gp.maxScreenCol) {
-                col = 0;
-                x = 0;
-                row++;
-                y += gp.tileSize;
+                int tileTypeId = tileCode / 10;
+                int tileIndex = tileCode % 10;
+
+                if (tileTypeId < tiles.size()) {
+
+                    ArrayList<Tile> tileSet = tiles.get(tileTypeId);
+
+                    if(tileIndex < tileSet.size()) {
+
+                        g2.drawImage(tileSet.get(tileIndex).image, col * gp.tileSize, row * gp.tileSize, gp.tileSize, gp.tileSize, null);
+
+                    }
+
+                }
+
             }
         }
     }
